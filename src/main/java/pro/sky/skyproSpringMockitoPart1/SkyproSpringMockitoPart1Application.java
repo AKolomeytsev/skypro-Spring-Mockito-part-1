875ee2815a3dd640cfb3537498c59db428a6742a
AKolomeytsev <a.kolomeytsev@list.ru>
2022-01-11 21:39:32 +0300

    Initial commit

---
+++ src/main/java/pro/sky/skyproSpringMockitoPart1/SkyproSpringMockitoPart1Application.java
@@ -0,0 +1,13 @@
+package pro.sky.skyproSpringMockitoPart1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringMockitoPart1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringMockitoPart1Application.class, args);
+	}
+
+}
